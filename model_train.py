3137f910824d36ef4a4b0c4953d8db75a049f72c
myidispg <myidispg@gmail.com>
2019-01-05 19:03:02 +0530

    Worked on the model and cleaned up some code

--- model_train.py
+++ model_train.py
@@ -30,18 +30,9 @@ validation_size = 0.2
 
 X_train, X_valid, y_train, y_valid = train_test_split(X_train, y_train, test_size=validation_size, random_state=0)
 
-# Feature Scaling
-sc = StandardScaler()
-X_train = sc.fit_transform(X_train)
-X_valid = sc.transform(X_valid)
-
 # Define the ANN.
 from keras.models import Sequential
 from keras.layers import Dense, Dropout
-from keras.optimizers import Adam, Adagrad
-from keras import regularizers
-
-from keras.layers.advanced_activations import LeakyReLU
 
 def model():
     model = Sequential()
@@ -57,24 +48,6 @@ def model():
 #    model.compile(optimizer=optimizer, loss = 'categorical_crossentropy', metrics = ['accuracy'])
     
     return model
-
-#def model():
-#    model = Sequential()
-#    model.add(Dense(128, input_dim = 172))
-#    model.add(LeakyReLU(alpha=0.1))
-##    model.add(Dropout(0.5))
-#    model.add(Dense(64, ))
-#    model.add(LeakyReLU(alpha=0.1))
-##    model.add(Dropout(0.5))
-#    model.add(Dense(32,))
-#    model.add(LeakyReLU(alpha=0.1))
-##    model.add(Dropout(0.5))
-#    model.add(Dense(7,  activation='sigmoid'))
-#    optimizer = Adam(lr=0.001, clipnorm=0.9)
-#    model.compile(optimizer='adam', loss = 'categorical_crossentropy', metrics = ['accuracy'])
-#    model.compile(optimizer=optimizer, loss = 'categorical_crossentropy', metrics = ['accuracy'])
-#    
-#    return model
 
 model = model()
 print(model.summary())
@@ -93,80 +66,25 @@ model.save('ann-emotion-recognition.h5')
 # ANN did not work, try Logistic Regressionfit
 from sklearn.linear_model import LogisticRegression
 classifier = LogisticRegression(random_state = 0)
-#X_train = X_train.reshape(172, 8196)
-#y_train = y_train.reshape(7, 8196)
-y_train = np.ravel(y_train)
-history = classifier.fit(X_train, y_train)
+classifier.fit(X_train, y_train)
 
+import pickle
+filename = 'logistic-emotion-recognition.sav'
+pickle.dump(classifier, open(filename, 'wb'))
+
+# load the model from disk
+loaded_model = pickle.load(open(filename, 'rb'))
+result = loaded_model.score(X_test, Y_test)
 # ---- Test on a real world image
-import os
 import cv2
-
-casc_directory = 'face-cascades/'
-
-def find_face(image_path):
-    # Create the haar cascade
-    faceCascade1 = cv2.CascadeClassifier(os.path.join(casc_directory, 'haarcascade_frontalface_alt.xml'))
-    faceCascade2 = cv2.CascadeClassifier(os.path.join(casc_directory, 'haarcascade_frontalface_alt2.xml'))
-    faceCascade3 = cv2.CascadeClassifier(os.path.join(casc_directory, 'haarcascade_frontalface_alt_tree.xml'))
-    faceCascade4 = cv2.CascadeClassifier(os.path.join(casc_directory, 'haarcascade_frontalface_default.xml'))
-    # Read the image
-    image = cv2.imread(image_path)
-    
-    # FInd image dimensions
-    size = image.shape
-    size = size[1] if size[1] < size[0] else size[0]
-    
-    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    # Detect faces in the image using 4 different classifiers
-    faces1 = faceCascade1.detectMultiScale(
-                gray, 
-                scaleFactor=1.2,
-                minNeighbors=5
-                )
-    faces2 = faceCascade2.detectMultiScale(
-                gray, 
-                scaleFactor=1.2,
-                minNeighbors=5
-                )
-    faces3 = faceCascade3.detectMultiScale(
-                gray, 
-                scaleFactor=1.2 ,
-                minNeighbors=5
-                )
-    faces4 = faceCascade4.detectMultiScale(
-                gray, 
-                scaleFactor=1.2,
-                minNeighbors=5
-                )
-    
-    # Go over the detected faces of all the classifiers and select one.
-    if len(faces1) == 1:
-        faces = faces1
-    elif len(faces2) == 1:
-        faces = faces2
-    elif len(faces3) == 1:
-        faces = faces3
-    elif len(faces4) == 1:
-        faces = faces4
-    else:
-        print('No faces found')
-        return None
-        
-    return faces
-#    # Return new image
-#    for (x, y, w, h) in faces:
-#        new_image = gray[y:y+h+20, x:x+w+20]
-#    
-#    return new_image
-
 # Load the required stuff for landmark detection.
 from imutils import face_utils
 import dlib
 from landmarks_calculation import left, right
 
-image_path = '/home/myidispg/My Files/Machine-Learning-Projects/Emotion-Dataset/cohn-kanade-images/S081/005/S081_005_00000019.png'
-#image_path = '../Google Photos/2018/00000PORTRAIT_00000_BURST20180407170851895.jpg'
+image_path = '/home/myidispg/My Files/Machine-Learning-Projects/Emotion-Dataset/cohn-kanade-images/S999/003/S999_003_00000055.png'
+#image_path = '../Google Photos/2018/00100sPORTRAIT_00100_BURST20180619133945605_COVER.jpg'
+#image_path = 'angry-image.jpeg'
 
 p = "shape_predictor_68_face_landmarks.dat"
 face_detector = dlib.get_frontal_face_detector()
@@ -174,13 +92,7 @@ landmark_predictor = dlib.shape_predictor(p)
 
 #Find face box in the image
 image = cv2.imread(image_path, 0)
-rects = find_face(image_path)
-rects = dlib.rectangle(left = rects[0][0], top=rects[0][1], right=rects[0][2], bottom=rects[0][3])
-# Find landmark
-shape = landmark_predictor(image, rects)
-# Convert to numpy array
-shape = face_utils.shape_to_np(shape)
-
+image = cv2.resize(image, (490, 640))
 
 rects = face_detector(image)
 for (i, rect) in enumerate(rects):
@@ -199,12 +111,7 @@ for i in range(len(left)):
 x_coords = np.asarray(x_coords)
 y_coords = np.asarray(y_coords)
 distance_between = np.sqrt(np.square(x_coords) + np.square(y_coords))
-average_coords = ((x_coords + y_coords)/2).reshape(1, -1)
-
-# Feature scaling
-test_x = sc.transform(distance_between.reshape(1, -1))
-
 
 model = keras.models.load_model('ann-emotion-recognition.h5')
-predict = classifier.predict(distance_between.reshape(1, -1))
+predict = model.predict(distance_between.reshape(1, -1))
     
